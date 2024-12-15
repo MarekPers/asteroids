d7797deb1c8f3e85e9cbf3fee788ad29beceb8d5
MarekPers <praca.m.pers@gmail.com>
2024-12-15 20:33:19 +0100

    Added initial display window

--- main.py
+++ main.py
@@ -3,11 +3,25 @@
 # throughout this file
 import pygame
 from constants import *
+from time import sleep
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    # running = True
+    color = (0, 0, 0)
+    while running:
+        for event in pygame.event.get():
+           if event.type == pygame.QUIT:
+               return
+        screen.fill(color)
+        pygame.display.flip()
+        sleep(1)
+
 
 if __name__ == "__main__":
     main()
