a2c95b1d0f096d962c4516efb97574f2c5cb9483
MarekPers <praca.m.pers@gmail.com>
2024-12-15 20:39:47 +0100

    code cleanup

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 from time import sleep
 
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     
-    # running = True
+    running = True
     color = (0, 0, 0)
     while running:
         for event in pygame.event.get():
            if event.type == pygame.QUIT:
-               return
+               running = False
         screen.fill(color)
         pygame.display.flip()
         sleep(1)
