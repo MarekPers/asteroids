4e252fffbdc999dca4ca12a5a07d0296af2565ab
MarekPers <praca.m.pers@gmail.com>
2024-12-16 19:57:19 +0100

    time lib replaced with pygame clock

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pygame
 from constants import *
-from time import sleep
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     running = True
@@ -15,7 +16,7 @@ def main():
                running = False
         screen.fill(color)
         pygame.display.flip()
-        sleep(1)
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
