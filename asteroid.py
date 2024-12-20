73896f8c06fee97200cba2d082e76c1304b4fb42
MarekPers <praca.m.pers@gmail.com>
2024-12-20 19:24:46 +0100

    added bullet collision & asteroid split

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
 from circleshape import CircleShape
+from constants import *
+import random
 
 
 class Asteroid(CircleShape):
@@ -12,4 +14,13 @@ class Asteroid(CircleShape):
     def update(self, dt):
         self.position += self.velocity * dt
 
-    
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        asteroid_1 = Asteroid(self.position.x, self.position.y, self.radius - ASTEROID_MIN_RADIUS)
+        asteroid_2 = Asteroid(self.position.x, self.position.y, self.radius - ASTEROID_MIN_RADIUS)
+        asteroid_spread = random.uniform(20, 50)
+        asteroid_1.velocity = self.velocity.rotate(asteroid_spread) * 1.2
+        asteroid_2.velocity = self.velocity.rotate(-asteroid_spread) * 1.2
